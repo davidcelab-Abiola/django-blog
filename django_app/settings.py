e72bbe0083f9f112a6fe8519208ba9f4fbbb830f
Abiola David <adedejisolomon549@gmail.com>
2026-06-03 06:59:54 +0100

    Enable real email

--- django_app/settings.py
+++ django_app/settings.py
@@ -113,4 +113,12 @@ LOGOUT_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 # EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'davidcelab@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('qmarlxkykzrvaspk')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
