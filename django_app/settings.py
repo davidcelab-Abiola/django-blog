dc0871121b3d207777f5158b724711db577b05de
Abiola David <adedejisolomon549@gmail.com>
2026-06-05 17:08:00 +0100

    add a vatiable to EMAIL_HOST_PASSWORD

--- django_app/settings.py
+++ django_app/settings.py
@@ -120,5 +120,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'davidcelab@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('qmarlxkykzrvaspk')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
