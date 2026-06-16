b7c8df84dae3d1b2a4f7923ad011930d81859a59
Abiola David <adedejisolomon549@gmail.com>
2026-06-16 03:05:36 +0100

    Added auto superuser creation in apps.py

--- users/apps.py
+++ users/apps.py
@@ -6,3 +6,19 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         import users.signals
+
+        from django.contrib.auth.models import User
+        from django.db.utils import OperationalError
+
+        try:
+            # Create superuser automatically if it doesn't exist
+            if not User.objects.filter(username='gmiadmin').exists():
+                User.objects.create_superuser(
+                    username='David',
+                    email='davidcelab@gmail.com',
+                    password='osapolo;'
+                )
+                print("Superuser created successfully.")
+        except OperationalError:
+            # Ignore during migrations
+            pass
